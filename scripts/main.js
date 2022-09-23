4682396fa6873f8af955b98a04c2f2fc95c247de
yelamin2 <yelamin2@yahoo.com>
2022-09-23 00:07:09 -0400

    Soted List

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,11 @@ var totalprice = pikprice.reduce(function(num, i){
 var averageprice = totalprice/pikprice.length;
 
 console.log( 'Average price for all items is '+ averageprice.toFixed(2));
-   
+ //////////
+       ///   
+ ////////
+ ///
+ /////////  
  
 
 // filters items by price between 14 and 18.
@@ -21,25 +25,69 @@ var priceRange = items.filter(function(item){
 console.log(priceRange);
 // let rangitems = priceRange
 //  return console.log(priceRange.map)
+
+///////////
+        ////
+        ////
+///////////
+        ////
+//////////
+
+
+//filter throught he currency code to find the matching value of GBP
 var isnotdollar = items.filter(function(item){
     return (item.currency_code == "GBP");
 });
 var itemnotindollars = isnotdollar.map(function(item){
-    return isnotdollar.title;
+    return item.title;
 });
-console.log(itemnotindollars, isnotdollar);
+console.log(isnotdollar, itemnotindollars);
+    ///
+   ///  //
+  ///  //
+  ////////
+     //
 
 // items made of wood
 
-var isitwood = items.map(function(item){
-    return (item.materials, item.title );
+var isitwood = items.filter(function(item){
+    return item.materials.includes("wood");
 });
-var itemofwood = isnotdollar.find(function(item){
-    return ;
+var itemofwood = isitwood.map(function(item){
+    return item.title;
 });
-console.log(itemnotindollars, isitwood);
+console.log(itemofwood);
+
+////////
+///
+////////
+    ///
+//////
 
 
 //Items that are ,made of eight materials or more
 
 
+var eightmaterialsplus = items.filter(function(item){
+    return item.materials.length >= 8;
+});
+
+;
+
+eightmaterialsplus.forEach(function(item){
+    console.log(item.title + 'has'+ item.materials.lenght);
+    console.log('Materials:' + item.materials);
+})
+
+
+///////
+//
+///////
+//  ///
+//////
+
+var ismadbyseller = items.filter(function(item){
+    return item.who_made === 'i_did';
+});
+
+console.log(`${ismadbyseller.length} items were Made by their sellers.`);
